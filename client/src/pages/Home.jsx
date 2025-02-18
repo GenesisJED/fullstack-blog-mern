5b3880a918c7bfefc05d465373203592772ad88f
genesis <escalonagenesis97@gmail.com>
2025-02-18 14:28:29 -0400

    change banner image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import CallToAction from '../components/CallToAction';
 import { useEffect, useState } from 'react';
 import PostCard from '../components/PostCard';
-import banner from '../assets/images/banner.jpg';
+import banner from '../assets/images/banner2.jpg';
 
 export const Home = () => {
 
