e33df3bf61985d56d0262122e751fc810cc4d96d
genesis <escalonagenesis97@gmail.com>
2025-02-19 09:47:22 -0400

    change category

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(5000, () => {
+app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
 
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -86,10 +86,10 @@ export const CreatePost = () => {
                             setFormData({ ...formData, category: e.target.value })
                         }
                     >
-                        <option value='uncategorized'>Select a category</option>
-                        <option value='javascript'>JavaScript</option>
-                        <option value='reactjs'>React.js</option>
-                        <option value='nextjs'>Next.js</option>
+                        <option value='Uncategorized'>Select a category</option>
+                        <option value='JavaScript'>JavaScript</option>
+                        <option value='React.js'>React.js</option>
+                        <option value='Next.js'>Next.js</option>
                     </Select>
                 </div>
                 <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
